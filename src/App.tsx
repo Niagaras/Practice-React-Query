2dce70e84ba076c2521e79fa06362c747a27dcba
Nigar Asadova <nigar.asadova@azintelecom.az>
2026-02-26 15:21:00 +0400

    adding pagenation into data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 //import CompletedTasks from './components/CompletedTasks'
 import OneUserTasks from './components/OneUserTasks'
-// import QueryBasics from './components/queryBasics'
+import QueryBasics from './components/QueryBasics'
 
 
 function App() {
@@ -10,9 +10,9 @@ function App() {
   return (
     <>
       <h1>Query App</h1>
-     {/* <QueryBasics /> */}
+     <QueryBasics />
      {/* <CompletedTasks /> */}
-     <OneUserTasks />
+     {/* <OneUserTasks /> */}
     </>
   )
 }
--- src/components/QueryBasics.tsx
+++ src/components/QueryBasics.tsx
@@ -1,5 +1,6 @@
 
 import {useQuery} from '@tanstack/react-query'
+import { useState } from 'react'
 
 export interface Todo{
     userId : number,
@@ -10,11 +11,14 @@ export interface Todo{
 
 
 export default function QueryBasics() {
-    const {isPending , isError , data, error} = useQuery<Todo[], Error>({
-        queryKey: ['todos'],
-        queryFn: fetchTodos,
+    const[page, setPage] = useState<number>(0);
+    const {isPending , isError , data, error, isFetching, isPlaceholderData} = useQuery<Todo[], Error>({
+        queryKey: ['todos', page],
+        queryFn:() => fetchTodos(page),
+        placeholderData: (previousData) => previousData,
     })
- 
+    
+
     if(isPending) return <div>Loading...</div>
     if(isError) return <div>`error is ${error.message}`</div>
 
@@ -28,13 +32,19 @@ export default function QueryBasics() {
                     {item.todo}
                 </li>
             ))}
+            <button onClick={() => setPage((old) => Math.max((old - 1), 0))} disabled={page === 0}>Previous 5</button>
+            <button onClick={
+                () => !isPlaceholderData && data&& setPage((old) => old + 1)
+               }> Next 5</button>
         </ul>
     </>
   )
 }
 
-export const fetchTodos = async():Promise<Todo[]> => {
-    const response = await fetch('https://dummyjson.com/todos');
+export const fetchTodos = async(page : number):Promise<Todo[]> => {
+    const limit = 5;
+    const skip = page * limit;
+    const response = await fetch(`https://dummyjson.com/todos?limit=${limit}&skip=${skip}`);
     const result = await response.json();
     return result.todos;
 }
